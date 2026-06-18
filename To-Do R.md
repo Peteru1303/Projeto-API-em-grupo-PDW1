f258ac194004a8d747075bc5ea3ffa37ff633111
RafaGR <110624346+RaffaGR@users.noreply.github.com>
2026-06-17 22:25:37 -0300

    natural ignorar kk

--- To-Do R.md
+++ To-Do R.md
@@ -1,3 +1,4 @@
 Concluidomodel notaFiscal, estoque, carro e vendedor:
 - Refatorar para tornar mais simples
 
+O repository vendedor foi concluido
--- src/repositories/vendedorRepository.ts
+++ src/repositories/vendedorRepository.ts
@@ -1,8 +1,9 @@
+import { executarComandoSQL } from "../database/mysql";
 import { Vendedor } from "../models/Vendedor";
 
 export class VendedorRepositorio {
     private static instance: VendedorRepositorio;
-    private vendedorList: Vendedor[] = [];
+    private vendedorList: Vendedor[] = []; // talvez nao vai precisar disso
 
     private constructor() { };
 
@@ -13,37 +14,61 @@ export class VendedorRepositorio {
         return this.instance;
     }
 
-    novoVendedor(vendedor: Vendedor): void {
-        this.vendedorList.push(vendedor);
+    static getCreateTableQuery(): string {
+        return `
+        CREATE TABLE IF NOT EXISTS Vendedor (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            nome VARCHAR(255) NOT NULL,
+            matricula VARCHAR(14) NOT NULL,
+            comissao_percentual VARCHAR(20) NOT NULL
+        );
+        `;
     }
 
 
-    listarVendedor(): Vendedor[] {
-        return this.vendedorList;
+    async novoVendedor(vendedor: Vendedor): Promise<Vendedor> {
+        const resultado = await executarComandoSQL(
+            "INSERT INTO Cliente (nome, matricula, comissao_percentual) VALUES (?, ?, ?)",
+            [vendedor.nome, vendedor.matricula, vendedor.comissao_percentual]
+        )
+
+        const idGerado = resultado.insertId;
+
+        const newVendedor = new Vendedor(idGerado, vendedor.nome, vendedor.matricula, vendedor.comissao_percentual);
+        
+        console.log('Novo vendedor inserido com sucesso:', newVendedor);
+        return newVendedor;
     }
 
-    buscarPorID(id: number): Vendedor | undefined {
-        return this.vendedorList.find(vendedor => vendedor.id === id);
+
+    async listarVendedor(): Promise<Vendedor[]> {
+        const linhas = await executarComandoSQL("SELECT * FROM Vendedor", []);
+                
+        const vendedor: Vendedor[] = linhas.map((linha: any) => {
+            return new Vendedor(linha.id, linha.nome, linha.matricula, linha.comissao_percentual);
+        });
+
+        return vendedor;
     }
 
-    atualizarVendedor(vendedorData: any, idAlt: number): void {
-        let vendedor: Vendedor | undefined = this.buscarPorID(idAlt);
-
-        if (vendedor) {
-            vendedor.nome = vendedorData.nome;
-            vendedor.matricula = vendedorData.matricula;
-            vendedor.comissao_percentual = vendedorData.comissao_percentual;
-        }
+    async buscarPorID(id: number): Promise<Vendedor | null> {
+        const linhas = await executarComandoSQL("SELECT * FROM Vendedor WHERE id = ?", [id]);
+                
+        if (linhas.length === 0) return null;
+        
+        const linha = linhas[0];
+        return new Vendedor(linha.id, linha.nome, linha.cpf, linha.comissao_percentual);
     }
 
-    removerVendedor(idRem: number): void {
-        let vendedor: Vendedor | undefined = this.buscarPorID(idRem);
+    async atualizarVendedor(vendedorData: any, id: number): Promise<void> {
+        await executarComandoSQL(
+            "UPDATE Vendedor SET nome = ?, matricula = ?, comissao_percentual = ? WHERE id = ?",
+            [vendedorData.nome, vendedorData.matricula, vendedorData.comissao_percentual, id]
+        );
+    }
 
-        if(vendedor){
-            let index = this.vendedorList.indexOf(vendedor);
-            this.vendedorList.splice(index, 1);
-            /* ou pop */
-        }
+    async removerVendedor(id: number): Promise<void> {
+        await executarComandoSQL("DELETE FROM Cliente WHERE id = ?", [id]);
     }
     //Adicionar função de retornar as notas fiscais de um vendedor
     //possivelmente reutilizar a função busca id e exportar a nota fiscal do vendedor?
